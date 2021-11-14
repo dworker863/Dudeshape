07aa9b5e1c2ab842cae3833b4ba63454848423cf
dworker <dworker863@gmail.com>
2021-11-14 12:07:34 +0600

    Desktop. Added dynamic slider to review block

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,45 @@
 $(window).on('load', function () {
+  const sliderWidth = $('.review__slider .review__item').length * 490 + 'px';
+  let sliderLeft = +$('.review__slider').css('left').slice(0, -2);
+
+  const getArrowDisable = () => {
+    const width = $('.review__slider').width();
+    sliderLeft = +$('.review__slider').css('left').slice(0, -2);
+
+    if (sliderLeft <= 0 && sliderLeft >= -490) {
+      $('.arrows__prev').addClass('arrow__disabled');
+    } else {
+      $('.arrows__prev').removeClass('arrow__disabled');
+    }
+
+    if (width + sliderLeft <= 980) {
+      $('.arrows__next').addClass('arrow__disabled');
+    } else {
+      $('.arrows__next').removeClass('arrow__disabled');
+    }
+  };
+
+  const getSliderTranslate = (translateValue, self) => {
+    sliderLeft = +$('.review__slider').css('left').slice(0, -2);
+    console.log(self.css('color'));
+
+    if (
+      self.attr('id') === 'arrows__prev' &&
+      self.css('color') === 'rgb(122, 122, 122)'
+    ) {
+      return;
+    } else if (
+      self.attr('id') === 'arrows__next' &&
+      self.css('color') === 'rgb(122, 122, 122)'
+    ) {
+      return;
+    }
+
+    $('.review__slider').css('left', sliderLeft + translateValue + 'px');
+  };
+
+  $('.review__slider').css('width', sliderWidth);
+
   $('.hamburger-wrapper').on('click', function (e) {
     const self = $(this);
     $(self).toggleClass('active');
@@ -7,7 +48,18 @@ $(window).on('load', function () {
 
   $('.prices__services-item input').on('click', function (e) {
     const self = $(this);
-    console.log('click');
     $(self).next('.services-item__checkbox').toggleClass('active');
+  });
+
+  $('.arrows__next').on('click', function (e) {
+    e.preventDefault();
+    getSliderTranslate(-490, $(this));
+    getArrowDisable();
+  });
+
+  $('.arrows__prev').on('click', function (e) {
+    e.preventDefault();
+    getSliderTranslate(490, $(this));
+    getArrowDisable();
   });
 });
